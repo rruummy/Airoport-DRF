0ca641ade9f79e806df89ea46da6566d77984adf
Roman Andrushchenko <romanandrushchenko0@gmail.com>
2026-07-22 16:13:02 +0300

    Connect drf-spectecular

--- airport/airport/urls.py
+++ airport/airport/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 from api import views
 from rest_framework import routers
@@ -32,4 +33,7 @@ urlpatterns = [
     path("", include(router.urls)),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-]
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/docs/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    ]
