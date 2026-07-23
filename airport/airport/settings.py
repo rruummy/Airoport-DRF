e5a220d853cdd1fe6a3b25f126bc4d9cc78ecbf8
Roman Andrushchenko <romanandrushchenko0@gmail.com>
2026-07-23 13:52:08 +0300

    Refactor api into user, flights, tickets

--- airport/airport/settings.py
+++ airport/airport/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api.apps.ApiConfig',
     'rest_framework',
     'drf_spectacular',
     'drf_spectacular_sidecar',
--- airport/airport/urls.py
+++ airport/airport/urls.py
@@ -42,12 +42,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('rest_framework.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path(
-        'api/docs/swagger/',
-        SpectacularSwaggerView.as_view(url_name='schema'),
-        name='swagger-ui',
-    ),
-    path(
-        'api/docs/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'
-    ),
+    path('api/docs/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/docs/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
