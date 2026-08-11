fb781e87df75549914359c61cd4180740433e4a6
Roman Andrushchenko <romanandrushchenko0@gmail.com>
2026-08-11 23:29:30 +0300

    Add async sending emails

--- airport/emails/tasks.py
+++ airport/emails/tasks.py
@@ -2,7 +2,8 @@ from celery import shared_task
 from django.utils import timezone
 
 from emails.models import EmailVerificationCode, PasswordResetCode
-
+from django.core.mail import send_mail
+from django.conf import settings
 
 @shared_task
 def delete_email_expired_codes():
@@ -19,3 +20,23 @@ def delete_password_expired_codes():
     ).delete()
 
     print(f"Deleted {deleted_count} expired password reset codes.")
+
+@shared_task
+def send_email_async(subject, message, recipient):
+    send_mail(
+        subject=subject,
+        message=message,
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        recipient_list=[recipient],
+        fail_silently=False,
+    )
+
+@shared_task
+def send_verification_email(email, code):
+    send_mail(
+        subject="Email verification",
+        message=f"Your verification code is: {code}",
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        recipient_list=[email],
+        fail_silently=False,
+    )
